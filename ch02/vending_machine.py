c26f7edb876460687d2d3e373af1c4e48304df0e
cashew <42924021+cashev@users.noreply.github.com>
2020-10-09 22:54:40 +0900

    forループで重複を削除

--- ch02/vending_machine.py
+++ ch02/vending_machine.py
@@ -3,40 +3,9 @@ product_price = input('product: ')
 change = int(insert_price) - int(product_price)
 print(change)
 
-# 5000円札の枚数を求める
-r5000 = change // 5000
-q5000 = change % 5000
-print('5000: ' + str(r5000))
+coin = [5000, 1000, 500, 100, 50, 10, 5, 1]
 
-# 1000円札の枚数を求める
-r1000 = q5000 // 1000
-q1000 = q5000 % 1000
-print('1000: ' + str(r1000))
-
-# 500円玉の枚数を求める
-r500 = q1000 // 500
-q500 = q1000 % 500
-print('500: ' + str(r500))
-
-# 100円玉の枚数を求める
-r100 = q500 // 100
-q100 = q500 % 100
-print('100: ' + str(r100))
-
-# 50円玉の枚数を求める
-r50 = q100 // 50
-q50 = q100 % 50
-print('50: ' + str(r50))
-
-# 10円玉の枚数を求める
-r10 = q50 // 10
-q10 = q50 % 10
-print('10: ' + str(r10))
-
-# 5円玉の枚数を求める
-r5 = q10 // 5
-q5 = q10 % 5
-print('5: ' + str(r5))
-
-# 5円玉の枚数を求める
-print('1: ' + str(q5))
+for i in coin:
+    r = change // i
+    change %= i
+    print(str(i) + ': ' + str(r))
