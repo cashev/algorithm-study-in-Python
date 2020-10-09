4f7cc72b0560548512a428def441f798026e8db5
cashew <42924021+cashev@users.noreply.github.com>
2020-10-09 23:33:29 +0900

    任意の基数で変換

--- ch02/convert.py
+++ ch02/convert.py
@@ -1,9 +1,14 @@
 n = 18
 
-result = ''
+def convert(n, base):
+    result = ''
 
-while n > 0:
-    result = str(n % 2) + result
-    n //= 2
+    while n > 0:
+        result = str(n % base) + result
+        n //= base
 
-print(result)
+    return result
+
+print(convert(n, 2))
+print(convert(n, 3))
+print(convert(n, 8))
