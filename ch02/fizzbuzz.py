5ac338c7b301a036ffa33d0ce8e9b80630e082e6
cashew <42924021+cashev@users.noreply.github.com>
2020-10-09 22:22:09 +0900

    fix typo

--- ch02/fizzbuzz.py
+++ ch02/fizzbuzz.py
@@ -1,9 +1,9 @@
 for i in range(1, 51):
     if i % 15 == 0:
-        print('fizzbuzz', end=' ')
+        print('FizzBuzz', end=' ')
     elif i % 3 == 0:
         print('Fizz', end=' ')
     elif i % 5 == 0:
-        print('buzz', end=' ')
+        print('Buzz', end=' ')
     else:
         print(i, end=' ')
