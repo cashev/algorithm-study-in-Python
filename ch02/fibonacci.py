44934b616eb7eec114c20349f42bfce46551ad64
cashew <42924021+cashev@users.noreply.github.com>
2020-10-10 16:58:34 +0900

    forループでフィボナッチ数列を取得

--- ch02/fibonacci.py
+++ ch02/fibonacci.py
@@ -1,9 +1,9 @@
 memo = {1: 1, 2: 1}
 def fibonacci(n):
-    if n in memo:
-        return memo[n]
+    fib = [1, 1]
+    for i in range(2, n):
+        fib.append(fib[i - 2] + fib[i - 1])
 
-    memo[n] = fibonacci(n - 2) + fibonacci(n - 1)
-    return memo[n]
+    return fib[n - 1]
 
 print(fibonacci(6))
