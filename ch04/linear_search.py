b187a546fce4f9281381a79303828ba139aea4f5
cashew <42924021+cashev@users.noreply.github.com>
2020-10-10 18:06:12 +0900

    線形探索を関数化

--- ch04/linear_search.py
+++ ch04/linear_search.py
@@ -1,10 +1,8 @@
-data = [50, 30, 90, 10, 20, 70, 60, 40, 80]
-found = False
-for i in range(len(data)):
-    if data[i] == 40:
-        print(i)
-        found = True
-        break
+def linear_search(data, value):
+    for i in range(len(data)):
+        if data[i] == value:
+            return i
+    return -1
 
-if not found:
-    print('Not found')
+data = [50, 30, 90, 10, 20, 70, 60, 40, 80]
+print(linear_search(data, 40))
